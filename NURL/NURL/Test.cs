47360f97c27a00bfd58adc69ae667c03f91bc7b4
CCAZOT <ccazot95@live.fr>
2014-05-29 16:26:42 +0200

    commit

--- NURL/NURL/Test.cs
+++ NURL/NURL/Test.cs
@@ -24,8 +24,8 @@ namespace NURL
 		{
 		    
 		    var app = new CommandURL();
-            var commands = new string[] { "get", "-url", @"http://api.openweathermap.org/data/2.5/weather?q=paris&units=metric" };
-            var expectedString = @"{""coord"":{""lon"":-95.56,""lat"":33.66},""sys"":{""message"":0.0079,""country"":""US"",""sunrise"":1401362009,""sunset"":1401413157},""weather"":[{""id"":800,""main"":""Clear"",""description"":""sky is clear"",""icon"":""01d""}],""base"":""cmc stations"",""main"":{""temp"":19.75,""pressure"":1013,""humidity"":88,""temp_min"":19,""temp_max"":20},""wind"":{""speed"":2.6,""deg"":340},""clouds"":{""all"":1},""dt"":1401368393,""id"":4717560,""name"":""Paris"",""cod"":200}";
+            var commands = new string[] { "get", "-url", @"https://ccazot.github.io" };
+            var expectedString = @"<!DOCTYPE html><html><body><h1>Hello World</h1><p>I'm hosted with GitHub Pages.</p></body></html>"+"\n";
 
    
             app.DispatchArgs(commands);
